3d9672e0dd4bcb460c783f1ec9294848f51e0a53
Lays Xavier <113479525+llaysxavier@users.noreply.github.com>
2023-04-19 21:01:07 -0300

    Add files via upload

---
+++ AvaliacaoII/src/UML/Gerente.java
@@ -0,0 +1,29 @@
+package UML;
+
+import java.time.LocalDate;
+
+public class Gerente extends CargoDeConfianca{
+
+	public Gerente(String nome, String cpf, LocalDate dataNascimento, Genero genero, double salarioBase,
+			LocalDate dataAdmissao) {
+		super(nome, cpf, dataNascimento, genero, salarioBase, dataAdmissao, Bonificacao.GERENTE);
+	}
+
+	public double getSalarioFinal(double salarioBase) {
+		double salarioFinal = salarioBase + (salarioBase * Bonificacao.GERENTE.valor);
+		return salarioFinal;
+	}
+
+	@Override
+	public String toString() {
+		return  "\nGerente" + 
+				"\nNome = " + super.nome + 
+				"\nCpf = " + super.cpf + 
+				"\nGenero = " + super.genero.texto + 
+				"\nSalario Base = " + super.getSalarioBase() + 
+				"\nData de Admissao = " + super.getDataAdmissao() +
+				"\nSalario Final = " + Util.FormatarMoeda(getSalarioFinal(salarioBase));
+
+	}
+	
+}
---
+++ AvaliacaoII/src/UML/Main.java
@@ -0,0 +1,19 @@
+package UML;
+
+import java.time.LocalDate;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Motorista m1 = new Motorista("julio", "794.654.635-15", LocalDate.of(1997, 3, 21), Genero.MASCULINO, 5000, LocalDate.of(2023, 2, 11), "45698721");
+		Diretor d1 = new Diretor("Lays", "045.896.335-66", LocalDate.of(1997, 3, 24), Genero.FEMININO, 11000, LocalDate.of(2023, 12, 03));
+		Gerente g1 = new Gerente("Yuri", "794.653.152-99", LocalDate.of(2000, 1, 27), Genero.MASCULINO, 8000, LocalDate.of(2023, 11, 04));
+		
+		System.out.println(m1.toString());
+		System.out.println(d1.toString());
+		System.out.println(g1.toString());
+	
+		
+	}
+
+}
